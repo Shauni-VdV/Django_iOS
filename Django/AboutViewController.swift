b9712b22fbdf01223d16abd09e761abcf7f1d9b6
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-12-26 23:03:23 +0100

    Add basic layout & segues to storyboard

---
+++ Django/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  Django
+//
+//  Created by Shauni Van de Velde on 26/12/2019.
+//  Copyright © 2019 Shauni Van de Velde. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
