194f4fe0077ac2c440d364c9bb246f3e76b2838b
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-12-27 19:33:29 +0100

    Add CodingKeys to Model

--- Django/Model/Movie.swift
+++ Django/Model/Movie.swift
@@ -14,28 +14,53 @@ public struct MovieListResponse: Codable{
     public let totalResults: Int
     public let totalPages: Int
     public let results: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
 }
 
 // Movie class that has all attributes a Movie from the API does
-public struct Movie: Codable{
-    public let id: Int
-    public let title: String
-    public let backdropPath: String?
-    public let posterPath: String?
-    public let overview: String
-    public let releaseDate: Date
-    public let voteAverage: Double
-    public let voteCount: Int
-    public let tagline: String?
-    public let genres: String?
-    public let videos: String?
-    public let credits: String?
-    public let adult: Bool
-    public let runtime: Int?
+public struct Movie: Codable, Equatable{
+    
+    let posterPath: String?
+    let adult: Bool
+    let overview: String
+    let releaseDate: String
+    let genreIds: [Int]
+    let id: Int
+    let originalTitle: String
+    let originalLanguage: String
+    let title: String
+    let backdropPath: String?
+    let popularity: Double
+    let voteCount: Int
+    let video: Bool
+    let voteAverage: Double
     public var posterURL: URL {
         return URL(string: "https://image.tmdb.org/t/p/w500\(posterPath ?? "")")!
         }
     public var backdropURL: URL {
         return URL(string: "https://image.tmdb.org/t/p/original\(backdropPath ?? "")")!
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case posterPath = "poster_path"
+        case adult
+        case overview
+        case releaseDate = "release_date"
+        case genreIds = "genre_ids"
+        case id
+        case originalTitle = "original_title"
+        case originalLanguage = "original_language"
+        case title
+        case backdropPath = "backdrop_path"
+        case popularity
+        case voteCount = "vote_count"
+        case video
+        case voteAverage = "vote_average"
     }
 }
