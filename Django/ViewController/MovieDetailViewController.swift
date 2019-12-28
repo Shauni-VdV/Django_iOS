76bbab033ce9a10eb24694081bd8738d47456732
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-12-28 14:53:45 +0100

    Implement ViewController for detail page

--- Django/ViewController/MovieDetailViewController.swift
+++ Django/ViewController/MovieDetailViewController.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2019 Shauni Van de Velde. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    
+    @IBOutlet weak var BackDropImageView: UIImageView!
+    @IBOutlet weak var TitleLabel: UILabel!
+    
+    var movie: Movie!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = movie.title
+        
+        self.BackDropImageView.sd_setImage(with: movie.backdropURL,  placeholderImage: UIImage(named: "PlaceholderPoster"))
+        self.TitleLabel.text = movie.title
+    }
+    
+    
+}
