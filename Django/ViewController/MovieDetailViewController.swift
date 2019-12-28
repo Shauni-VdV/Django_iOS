3eb3b2b26836749f239a6cc151315648447841fb
Shauni Van de Velde <shauni.vandevelde@gmail.com>
2019-12-28 14:47:18 +0100

    Add basic detail view to storyboard (backdrop & title)

---
+++ Django/ViewController/MovieDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MovieDetailViewController.swift
+//  Django
+//
+//  Created by Shauni Van de Velde on 28/12/2019.
+//  Copyright © 2019 Shauni Van de Velde. All rights reserved.
+//
+
+import Foundation
